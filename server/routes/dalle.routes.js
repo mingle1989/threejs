bbd16ab3ee926a7202194e56108978beabc5c30f
MinglexMacabre <minglexmacabre@gmail.com>
2023-12-13 11:13:47 -0500

    fixes

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -7,7 +7,8 @@ dotenv.config();
 const router = express.Router();
 
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY, // This is also the default, can be omitted
+  organization: process.env.ORG_ID,
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 router.route('/').get((req, res) => {
@@ -18,14 +19,14 @@ router.route('/').post(async (req, res) => {
   try {
     const { prompt } = req.body;
 
-    const response = await openai.createImage({
+    const response = await openai.images.generate({
       prompt,
       n: 1,
       size: '1024x1024',
       response_format: 'b64_json',
     });
 
-    const image = response.data.data[0].b64_json;
+    const image = response.data[0].b64_json;
 
     res.status(200).json({ photo: image });
   } catch (error) {
