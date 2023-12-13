f52f57015e77d915cca5ccf6ced0bffc676d0ee2
MinglexMacabre <minglexmacabre@gmail.com>
2023-12-13 09:48:33 -0500

    fixed import

--- server/routes/dalle.routes.js
+++ server/routes/dalle.routes.js
@@ -1,16 +1,14 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
-import OpenAIApi from 'openai';
+import OpenAI from 'openai';
 
 dotenv.config();
 
 const router = express.Router();
 
-const config = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY, // This is also the default, can be omitted
 });
-
-const openai = new OpenAIApi(config);
 
 router.route('/').get((req, res) => {
   res.status(200).json({ message: 'Hello from DALL.E ROUTES' });
